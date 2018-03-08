bf34c8c0a1bf725eb2eb8f37d690eb3760495283
root <root@ip-172-31-86-104.ec2.internal>
2018-03-08 12:14:51 +0000

    instance with sg

--- main.tf
+++ main.tf
@@ -4,10 +4,32 @@ provider "aws"{
 }
 
 resource "aws_instance" "example" {
-	ami	= "ami-97785bed"
+	ami	= "ami-40d28157"
 	instance_type = "t2.micro"
+	vpc_security_group_ids = ["${aws_security_group.instance_sg.id}"]
+	user_data = <<-EOF
+		    #!/bin/bash
+		    echo "Hello, Shiva" > index.hrml
+		    nohup busybox httpd -f -p 8080 &
+		EOF
 
 	tags {
 	Name = "example-terraform"
  	}
 }
+
+resource "aws_security_group" "instance_sg" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+	value = "${aws_instance.example.public_ip}"
+}
+
