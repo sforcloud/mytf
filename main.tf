8360f27cba8f318aa564ab2816ba1f7d9fca2cf7
root <root@ip-172-31-86-104.ec2.internal>
2018-03-08 10:56:39 +0000

    code

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 #provider is aws other possible values can be azure gcp
 provider "aws"{
-	 access_key = "AKIAI2DQ7AS3GKRGRQNQ"
-	 secret_key = "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"
   	 region     = "us-east-1"
 }
 
